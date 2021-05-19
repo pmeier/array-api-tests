24e5b29d25316968f6f97c718c464311559c80bb
Philip Meier <github.pmeier@posteo.de>
2021-05-19 12:33:01 +0200

    update PyTorch helper

--- array_api_tests/_pytorch.py
+++ array_api_tests/_pytorch.py
@@ -3,6 +3,13 @@ import torch
 __all__ = ["array_module"]
 
 array_module = torch
+
 array_module.asarray = torch.as_tensor
-array_module.inf = torch.tensor(float("Inf"))
 array_module.equal = torch.eq
+
+array_module.e = torch.tensor(2.718281828459045)
+array_module.inf = torch.tensor(float("Inf"))
+array_module.nan = torch.tensor(float("NaN"))
+array_module.pi = torch.tensor(3.141592653589793)
+
+array_module.Tensor.size = property(lambda self: self.numel())
